e36541ba764a197ba3e2bd5e28f6f1608927e179
Persiteo <Persiteo@users.noreply.github.com>
2023-06-06 23:59:14 +0300

    add changes to moduls

--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,84 @@
+import datetime
 import json
 
 
-def read_json(file_path):
+def read_json(file_path) -> list:
     """
     Читает файл json и возвращает его содержимое
+    :param Абсолютный путь к файлу json
     :return: Данные в виде списка словарей
     """
-    with open(file_path, encoding='utf-8') as f:
-        data = json.load(f)
+    with open(file_path, 'r', encoding='utf-8') as f:
+        py_data = json.load(f)
 
-    return data
+    return py_data
 
 
-def sort_data(data):
+def sort_data(array):
     """
-    сортирует данные по статусу операции
-    :param data: исходные даннеы для сортировки
+    сортирует данные по дате и фильтрует по статусу операции EXECUTED
+    :param array: исходные данныe для сортировки
     :return: Отсортированные данные
     """
-    pass
+    clean_data = []
+    sorted_data = []
+
+    for item in array:
+        if "date" and "state" in item.keys():
+            clean_data.append(item)
+    clean_data.sort(key=lambda x: x["date"], reverse=True)
+
+    for item in clean_data:
+        if item["state"] == "EXECUTED":
+            sorted_data.append(item)
+
+    return sorted_data
 
 
-def bluring_cnumber(card_number):
+def modified_date(array: dict):
     """
-    Заменяет нужные цифры у номера карты на *
-    :param card_number: Исходный номер карты
-    :return: Маскированную строку
+    Преобразует формат вывода даты операции
+    :param array: Массив данных, cодержащий дату операции
+    :return: Исправленный формат даты операции
     """
-    pass
+    return datetime.datetime.strptime(array['date'], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
 
 
-def bluring_anumber(amount_number):
+
+
+def bluring_anumber(array: dict):
     """
-    Заменяет нужные цифры у номера счета на *
-    :param amount_number: Исходный номер счета
-    :return: Маскированную строку
+    Заменяет нужные цифры номера карты на *
+    :param array: Массив данных, cодержащий исходные номера карты
+    :return: Маскированные строки
     """
-    pass
+    if 'to' in array.keys():
+        blur_to = '**' + array['to'][-4::]
+    else:
+        blur_to = 'n/a'
+    return blur_to
+
+
+def bluring_cnumber(array: dict):
+    """
+    Заменяет нужные цифры номера cчёта на *
+    :param array: Массив данных, cодержащий исходные номера счета
+    :return: Маскированные строки
+    """
+    if 'from' in array.keys():
+        if 'Счет' in array['from']:
+            blur_from = f"{array['from'][:-16]}{'*' * 12}{array['from'][-4::]}"
+        else:
+            blur_from = f"{array['from'][:-12]} {array['from'][-12:-10]}** **** {array['from'][-4::]}"
+    else:
+        blur_from = 'n/a'
+    return blur_from
+
+
+def get_formated_info(array: dict) -> str:
+    """
+    Формируют по шаблону отчёт по операции
+    :param array: Массив данных
+    :return str: Отформатированные строки по шаблону
+    """
+    return f"{modified_date(array)} {array['description']} {bluring_cnumber(array)} -> {bluring_anumber(array)} {array['operationAmount']['amount']} {array['operationAmount']['currency']['name']}"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
-from src.utils import read_json, sort_data
+
+from src.utils import *
 
 
 def test_read_json(path_to_test_json, expected_result_for_read_json_test):
@@ -8,5 +9,18 @@ def test_read_json(path_to_test_json, expected_result_for_read_json_test):
 def test_sort_data(data_for_sort, expected_result_for_sort):
     assert sort_data(data_for_sort) == expected_result_for_sort
 
-# def test_bluring_cnumber():
-#     assert
+
+def test_modified_date():
+    assert modified_date({"date": "2018-09-12T21:27:25.241689"}) == "12.09.2018"
+
+
+def test_bluring_cnumber():
+    assert bluring_cnumber({"from": "Visa Platinum 1246377376343588"}) == "Visa Platinum 1246 37** **** 3588"
+
+
+def test_bluring_anumber():
+    assert bluring_anumber({"to": "Счет 75651667383060284188"}) == "**4188"
+
+
+def test_get_formated_info():
+    pass
